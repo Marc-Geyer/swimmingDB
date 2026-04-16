30b92f2e9958a4bb393ab67150a7d3439499a309
Marc Geyer <marc.geyer@proton.me>
2026-04-16 10:44:29 +0200

    debug trusted origins

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,9 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 
-CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
+CSRF_TRUSTED_ORIGINS = [str(s) for s in os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')]
+print(CSRF_TRUSTED_ORIGINS)
+print(type(CSRF_TRUSTED_ORIGINS))
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'Lax'
